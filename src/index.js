3685d73d00eca865e04071b1d43f1a495d6bfcdc
MOOW4 <108760540+MOOW4@users.noreply.github.com>
2023-09-27 22:54:12 +0200

    add some basic stuff

--- src/index.js
+++ src/index.js
@@ -2,3 +2,18 @@
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+
+import React from "react";
+import ReacdDOM from "react-dom";
+
+ReacdDOM.render(
+	<div>
+		<h1>My favourite foods</h1>
+		<ul>
+			<li>Pivo</li>
+			<li>Cigarety</li>
+			<li>Palenky</li>
+		</ul>
+	</div>,
+	document.getElementById("root")
+);
