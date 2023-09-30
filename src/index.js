59397a8416ed1a1154e489296f33c6bf4edb3ade
MOOW4 <108760540+MOOW4@users.noreply.github.com>
2023-09-30 22:30:26 +0200

    add styling

--- src/index.js
+++ src/index.js
@@ -6,14 +6,22 @@
 import React from "react";
 import ReacdDOM from "react-dom";
 
+const img = "https://picsum.photos/200";
 ReacdDOM.render(
 	<div>
-		<h1>My favourite foods</h1>
-		<ul>
-			<li>Pivo</li>
+		<h1 className="heading" contentEditable="true" spellCheck="false">
+			My favourite foods
+		</h1>
+		{/* 		<ul>
+			<li>Pivos</li>
 			<li>Cigarety</li>
 			<li>Palenky</li>
-		</ul>
+		</ul> */}
+		<div>
+			<img  className="image" src={img + "?grayscale"}></img>
+			<img  className="image" src={img + "?grayscale"}></img>
+			<img  className="image" src={img + "?grayscale"}></img>
+		</div>
 	</div>,
 	document.getElementById("root")
 );
